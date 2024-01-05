ff2fa923bcf359b816f8b4ba345a11de95532295
João Filippe Rossi Rodrigues <joaorodrigues@eulabs.com.br>
2024-01-05 15:23:04 -0300

    test: adding tests

--- merge_test.go
+++ merge_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"github.com/joaofilippe/americanas-desafio/models"
 )
 
 func Test_FromArrayToListNode(t *testing.T) {
@@ -30,10 +32,56 @@ func Test_FromArrayToListNode(t *testing.T) {
 
 	if list2.Next.Val != 6 {
 		t.Errorf("Expected 6, got %d", list2.Next.Val)
-	}	
+	}
 
 	if list2.Next.Next.Val != 8 {
 		t.Errorf("Expected 8, got %d", list2.Next.Next.Val)
-	}	
+	}
 
+}
+
+func Test_FromListNodeToArray(t *testing.T) {
+	a := &models.ListNode{
+		Val: 5,
+		Next: &models.ListNode{
+			Val: 6,
+			Next: &models.ListNode{
+				Val: 8,
+			},
+		}}
+
+	array := FromListNodeToArray(a)
+
+	if array[0] != 5 {
+		t.Errorf("Expected 5, got %d", array[0])
+	}
+
+	if array[1] != 6 {
+		t.Errorf("Expected 6, got %d", array[1])
+	}
+
+	if array[2] != 8 {
+		t.Errorf("Expected 8, got %d", array[2])
+	}
+
+}
+
+func Test_MergeList(t *testing.T) {
+	a := []int{2, 3, 4}
+	b := []int{1, 4, 6}
+
+	c := []int{1, 2, 3, 4, 4, 6}
+
+	listA := FromArrayToListNode(a)
+	listB := FromArrayToListNode(b)
+
+	merged := MergeListNode(listA, listB)
+
+	array := FromListNodeToArray(merged)
+
+	for i, v := range array {
+		if v != c[i] {
+			t.Errorf("Expected %d, got %d", c[i], v)
+		}
+	}
 }
